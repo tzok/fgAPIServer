6b2a746c410446404a37a1a98c221560a0dac7a2
Riccardo Bruno <riccardo.bruno@gmail.com>
2016-02-09 10:19:08 +0100

    Automated version number

--- fgapiserver_cfg.py
+++ fgapiserver_cfg.py
@@ -20,7 +20,7 @@
 __author__     = "Riccardo Bruno"
 __copyright__  = "2015"
 __license__    = "Apache"
-__version__    = "1.0"
+__version__    = "v0.0.1-434fd3c-1"
 __maintainer__ = "Riccardo Bruno"
 __email__      = "riccardo.bruno@ct.infn.it"
 
