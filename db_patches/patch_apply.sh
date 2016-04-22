781011f27d2edb21bec9b44cf5b73ca56175c877
Riccardo Bruno <riccardo.bruno@gmail.com>
2016-04-22 12:19:05 +0200

    Adding patch_apply.sh script to update automatically the DB

---
+++ db_patches/patch_apply.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+# 
+# patch_apply.sh - Apply available patches to APIServerDaemon DB
+#
+# Author: Riccardo Bruno <riccardo.bruno@ct.infn.it>
+#
+DEFAULTDBVER="0.0.1" # Baseline setup version
+
+ts() {
+	  date +%Y%m%d%H%M%S
+}
+
+out() {
+	  TS=$(ts)
+		    echo "$TS $*"
+}
+
+err() {
+	  TS=$(ts)
+		    echo "$TS $*" >&2
+}
+
+local_exec() {
+  OUT=$(mktemp)
+  ERR=$(mktemp)
+  $* >$OUT 2>$ERR
+  while read ln; do
+    out "$ln"
+  done < $OUT
+  while read ln; do
+    err "$ln"
+  done < $ERR
+  rm -f $OUT
+  rm -f $ERR
+}
+
+get_dbver() {
+  DBVER=$(ASDB_OPTS="-N -s"; asdb "select max(version) from db_patches;" 2>/dev/null)
+  if [ "$DBVER" = "" ]; then
+    DBVER=$DEFAULTDBVER
+  fi
+  echo $DBVER
+}
+
+
+#
+# Apply patches
+#
+
+# Get current DB version
+DBVER=$(get_dbver)
+out "Current DB version: $DBVER"
+# Build the list of patches to apply
+DBPATCHES=$(/bin/ls -1 *.sh |\
+		    grep -E '[0-9]+\.sh' |\
+			awk -F'_' '{ print $2 }' |\
+			awk -F'.sh' -v ver=$DBVER '{ if($1 > ver) print $1}')
+out "Selected patch versions: "$(echo $DBPATCHES | sed s/\ /,\ /g)
+# Apply selected patches
+COUNT=1
+STOPPED=0
+for ver in $DBPATCHES
+do
+  out "Executing: patch_${ver}.sh"
+  chmod +x patch_${ver}.sh
+  local_exec "./patch_${ver}.sh" > ./patch_${ver}.log
+  # Verify patch
+  DBVER=$(get_dbver)
+  if [ "$DBVER" != "$ver" ]; then
+    err "An error occurred while applying patch $ver"
+	err "Exiting"
+	STOPPED=1
+	break
+  else
+    COUNT=$((COUNT+1))
+  fi
+done
+# Report about execution
+if [ $STOPPED -ne 0 ]; then
+  err "Patching has been interrupted while executing patch_$ver.sh file"
+  err "Please verify its content and the patch_$ver.log file"
+  err "Then try to complete its execution manually and retry to"
+  err "apply patches"
+else
+  out "Applied $COUNT patches"
+  out "DB is now version $ver"
+  out "Done"
+fi
+
