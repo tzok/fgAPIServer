4fce76a1c2dc690f1647e38d6d05b67dd00eba0c
Riccardo Bruno <riccardo.bruno@gmail.com>
2016-04-22 16:27:00 +0200

    fixed stress_test code

--- apps/toscaTest/stress_test.sh
+++ apps/toscaTest/stress_test.sh
@@ -14,13 +14,16 @@ for i in $(seq 1 $SUBMIT_COUNT)
 do
   echo "Submitting job #$i"
   TMP=$(mktemp)
-  CURLDATA="'{\"application\":\"$APP_ID\",\"description\":\"tosca test run\"}'"
-  eval curl -i -H "Content-Type: application/json" -X POST -d $CURLDATA http://localhost:8888/v1.0/tasks?user=$USER | tee $TMP 
+  CMD="curl -i -H \"Content-Type: application/json\" -X POST -d '{\"application\":\""$APP_ID"\",\"description\":\"tosca test run\"}' http://localhost:8888/v1.0/tasks?user=$USER | tee $TMP"
+  eval $CMD
   id=$(cat $TMP | grep id | awk -F":" '{ print $2 }' | tr \" ' ' | awk '{print $1}' | xargs echo)
-  echo "id for job $i = $id"
-  curl -i -X POST http://localhost:8888/v1.0/tasks/$id/input?user=brunor
+  if [ "$id" != "" ]; then
+    echo "id for job $i = $id"
+    curl -i -X POST http://localhost:8888/v1.0/tasks/$id/input?user=brunor
+  fi
   rm -f $TMP
   if [ $i -ne $SUBMIT_COUNT ]; then
     sleep $SUBMIT_DELAY
   fi
 done
+
