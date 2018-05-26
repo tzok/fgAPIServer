08752b39a7faacff8027980fefda561f43599061
ricsx <ricsxm@hotmail.com>
2018-05-26 19:52:53 +0200

    mysql8 issue with the use of PASSWORD function, moving it to SHA

--- fgapiserver_db.sql
+++ fgapiserver_db.sql
@@ -263,11 +263,11 @@ create table fg_user (
 
 -- Users baseline values; users and passwords must be replaced with real values
 insert into fg_user (id,name,password,first_name,last_name,institute,mail,creation,modified)
-values (1,'futuregateway',password('futuregateway'),'FutureGateway','FutureGateway','INFN','sgw-admin@lists.indigo-datacloud.eu',now(),now());
+values (1,'futuregateway',sha('futuregateway'),'FutureGateway','FutureGateway','INFN','sgw-admin@lists.indigo-datacloud.eu',now(),now());
 insert into fg_user (id,name,password,first_name,last_name,institute,mail,creation,modified)
-values (2,'test',password('test'),'Test','Test','INFN','sgw-admin@lists.indigo-datacloud.eu',now(),now());
+values (2,'test',sha('test'),'Test','Test','INFN','sgw-admin@lists.indigo-datacloud.eu',now(),now());
 insert into fg_user (id,name,password,first_name,last_name,institute,mail,creation,modified)
-values (3,'brunor',password('brunor'),'Riccardo','Bruno','INFN','riccardo.bruno@ct.infn.it',now(),now());
+values (3,'brunor',sha('brunor'),'Riccardo','Bruno','INFN','riccardo.bruno@ct.infn.it',now(),now());
 
 
 -- Groups table, users may belong to one or more groups defined below
